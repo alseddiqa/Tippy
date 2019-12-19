d89530d5cafbce765eff94eaab7956538fca13f3
Abdullah Alseddiq <alseddiq.af@gmail.com>
2019-12-18 21:33:15 -0800

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Abdullah Alseddiq on 12/18/19.
+//  Copyright © 2019 Abdullah Alseddiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
